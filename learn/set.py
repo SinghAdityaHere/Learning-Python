95ead1afdf6f98f8503c418b61e30546d1e7bf1e
Aditya Singh <168402835+SinghAdityaHere@users.noreply.github.com>
2026-01-27 21:05:51 +0530

    Add files via upload

---
+++ learn/set.py
@@ -0,0 +1,12 @@
+# You are given a list of prog lang
+# ["python","java","c++","python","java","c"]
+# convert it into a set and print how many unique lang 
+
+progList=["python","java","c++","python","java","c"]
+print(type(progList))
+print(progList)
+
+progSet=set(progList)
+print(type(progSet))
+print(progSet)
+print("unique langs known: ", len(progSet))
