c1b3f2c7ffdd08f4955c376eb5cbbac168fbeca7
Aditya Singh <168402835+SinghAdityaHere@users.noreply.github.com>
2026-01-27 17:26:13 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+student={
+  "name": "cool" ,
+  "age" : 98 ,
+  "district": "darbhanga" ,
+  "state": "amsterdam" ,
+  "city": "chennai"
+}
+
+print(type(student))
+print(student["district"])
+print(student)
+
+student["district"]= "patna"
+print(student["district"])
+print(student)
+
+student["fruit"]="apple"
+print(student)
---
+++ list.py
@@ -0,0 +1,9 @@
+# take 3 food name from user and store it in a list
+
+f1 = input("Enter 1st food: ")
+f2 = input("Enter 2nd food: ")
+f3 = input("Enter 3rd food: ")
+
+fL=[f1,f2,f3]
+print(fL)
+print(len(fL))
