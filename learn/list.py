79157aab66c47078ae4c5c5faf6cecf929aca07b
Aditya Singh <168402835+SinghAdityaHere@users.noreply.github.com>
2026-01-29 12:33:20 +0530

    Update list.py

--- learn/list.py
+++ learn/list.py
@@ -1,3 +1,5 @@
+# list is ordered, start from 0,1,2 and so on
+
 # take 3 food name from user and store it in a list
 
 f1 = input("Enter 1st food: ")
@@ -6,4 +8,5 @@ f3 = input("Enter 3rd food: ")
 
 fL=[f1,f2,f3]
 print(fL)
+
 print(len(fL))
