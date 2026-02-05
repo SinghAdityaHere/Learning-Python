c108b05494a4846888903a1ad26ee151fc1c9f16
Aditya Singh <168402835+SinghAdityaHere@users.noreply.github.com>
2026-02-05 23:02:54 +0530

    Update WhileLoop.py

--- learn/WhileLoop.py
+++ learn/WhileLoop.py
@@ -14,15 +14,24 @@ while i>=1:
 
 # print even num between 1-50
 i=2
-
 while i<=50:
   print(i)
   i+=2
 
+
 # another one
 i=1
-
 while i<=50:
   if(i%2==0):
     print(i)
   i+=1
+
+
+# Write a program that prints the sum of first n natural numbers
+n= int(input("Enter a number:"))
+sum=0
+while n>=1:
+  sum+=n
+  n-=1
+
+print(f'Sum is: {sum}')
