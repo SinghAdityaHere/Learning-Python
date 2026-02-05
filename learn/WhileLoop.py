38169955eacfa4fd2fd9f1c516c233924e20260e
Aditya Singh <168402835+SinghAdityaHere@users.noreply.github.com>
2026-02-05 22:23:09 +0530

    Add files via upload

---
+++ learn/WhileLoop.py
@@ -0,0 +1,28 @@
+# # 1-10 while loop
+i=1
+while i<=10:
+  print(i)
+  i+=1
+
+
+# # 10-1 while loop
+i=10
+while i>=1:
+  print(i)
+  i-=1
+
+
+# print even num between 1-50
+i=2
+
+while i<=50:
+  print(i)
+  i+=2
+
+# another one
+i=1
+
+while i<=50:
+  if(i%2==0):
+    print(i)
+  i+=1
